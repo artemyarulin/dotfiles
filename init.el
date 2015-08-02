853cf02d238cafdc7a63f48b551579641f8d934a
Artem Yarulin <artem@unity3d.com>
2015-08-02 11:10:02 +0300

    Emacs config + Tmux config + Gitignore

---
+++ init.el
@@ -0,0 +1,44 @@
+;; Packages
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(setq package-enable-at-startup nil)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+
+;; Customization
+(setq-default indent-tabs-mode nil)
+(global-set-key (kbd "C-q") 'mark-sexp)
+
+
+;; UI
+(scroll-bar-mode -1)
+(setq inhibit-startup-screen t)
+(set-frame-parameter nil 'fullscreen 'fullboth)
+(custom-set-variables
+ '(custom-enabled-themes (quote (solarized-light)))
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
+
+
+;; Projectile + helm
+(add-hook 'after-init-hook #'projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(setq projectile-enable-caching nil)
+(helm-projectile-on)
+(helm-mode 1)
+
+
+;; cider
+(add-hook 'cider-mode-hook #'eldoc-mode)
+(setq nrepl-log-messages nil)
+(setq nrepl-hide-special-buffers t)
+(setq cider-show-error-buffer 'only-in-repl)
+(setq cider-prompt-save-file-on-load nil)
+
+
+;; js2
+(setq js2-strict-missing-semi-warning nil)
+(setq js2-missing-semi-one-line-override nil)
+(add-hook 'js-mode-hook 'js2-minor-mode)
