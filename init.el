a61e6634b3ac7fa23470bb98bb175a344ca4a285
Artem Yarulin <artem@unity3d.com>
2015-08-02 13:53:20 +0300

    Custom colors for terminal

--- init.el
+++ init.el
@@ -7,11 +7,8 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-
 ;; Customization
 (setq-default indent-tabs-mode nil)
-(global-set-key (kbd "C-q") 'mark-sexp)
-
 
 ;; UI
 (scroll-bar-mode -1)
@@ -19,11 +16,8 @@
 (setq inhibit-startup-screen t)
 (set-frame-parameter nil 'fullscreen 'fullboth)
 (custom-set-variables
- ;; For terminial the best is wheatgrass
- ;; Could't find a way to load it for terminal only
  '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
-
 
 ;; Projectile + helm
 (add-hook 'after-init-hook #'projectile-global-mode)
@@ -33,16 +27,25 @@
 (helm-mode 1)
 (setq helm-candidate-number-limit 1000)
 
-
-;; cider
+;; Cider
 (add-hook 'cider-mode-hook #'eldoc-mode)
 (setq nrepl-log-messages nil)
 (setq nrepl-hide-special-buffers t)
 (setq cider-show-error-buffer 'only-in-repl)
 (setq cider-prompt-save-file-on-load nil)
 
-
 ;; js2
 (setq js2-strict-missing-semi-warning nil)
 (setq js2-missing-semi-one-line-override nil)
 (add-hook 'js-mode-hook 'js2-minor-mode)
+
+;; Custom functions
+(defun termui ()
+  "For terminal we are using different colors"
+  (interactive)
+  (load-theme 'wheatgrass)
+  (set-face-attribute 'helm-selection nil :background "#A9A9A9"))
+
+;; Shortcuts
+(global-set-key (kbd "<f7>") 'termui)
+(global-set-key (kbd "C-q") 'mark-sexp)
